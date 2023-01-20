efbab2fb541e26125e4befcdf5e5b63e25cc3eeb
Alexandre <alexandre.revillon@outlook.fr>
2023-01-20 14:53:40 +0100

    methode de la puissance

---
+++ PageRank.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 20 14:05:07 2023
+
+@author: arevillo
+"""
+
+import numpy as np
+import scipy.linalg as nla
+
+
+# import csv
+
+# data = list()
+
+# with open("wikispeedia_paths-and-graph/paths_finished.tsv") as fd:
+#     rd = csv.reader(fd, delimiter="\t", quotechar='"')
+#     for row in rd:
+#         data.append(row)
+
+
+def norm(x):
+    somme = 0
+    for i in range(x.shape[0]):
+        somme += x[i]**2 
+    return np.sqrt(somme)
+
+
+def puissance(A,z0,tol,nitermax):
+    q = z0/norm(z0)
+    q2 = q
+    res = tol + 1
+    niter = 0
+    z = np.dot(A,q)
+    while ((res > tol) and (niter <= nitermax)):
+        q = z/norm(z)
+        z = np.dot(A,q)
+        lam = np.dot(q,z)
+        x1 = q
+        z2 = np.dot(q2,A)
+        q2 = z2/norm(z2)
+        y1 = q2
+        c = np.dot(y1,x1)
+        if c > 5E-2:
+            res = norm(z - lam*q)/c
+            niter = niter + 1
+            nu1=lam
+        else:
+            print("Problème de convergence !")
+            break
+    return(nu1,x1)
+
+
+A = np.array([[1,2,3],
+              [4,5,6],
+              [7,8,9]])
+print(nla.eigvals(A))
+
+print(puissance(A, np.array([1,1,1]), 0.0000001, 1000))
